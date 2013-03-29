428273a90a287b3bde6dff781c4c02b2bba628a6
Matthieu Moquet <matthieu@moquet.net>
2013-03-29 09:10:19 +0100

    Add basic license file

--- tests/EmailChecker/Tests/TestCase.php
+++ tests/EmailChecker/Tests/TestCase.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of EmailChecker.
+ *
+ * (c) Matthieu Moquet <matthieu@moquet.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace EmailChecker\Tests;
 
 class TestCase extends \PHPUnit_Framework_TestCase
